d7b039fa5871355c6bfe70bf4ebc2fdcb4ba4b15
Carly Tarett <carlytarett@hotmail.com>
2020-02-02 16:49:51 +0000

    Start point

--- handler.js
+++ handler.js
@@ -2,54 +2,141 @@ const express = require("express");
 const cors = require("cors");
 const serverlessHttp = require("serverless-http");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/tasks", function (request, response) {
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "todoDB"
+});
+ 
+// GET /tasks
 
-  console.log(request);
-  response.status(200).json({
-    tasks: [
-      { id: 1, description: "Task 1", status: "live", dateAdded: "2019-12-02" },
-      { id: 2, description: "Task 2 ", status: "live", dateAdded: "2019-12-02" },
-      { id: 3, description: "Task 3", status: "completed", dateAdded: "2019-12-02" }
+app.get("/tasks", function(request, response) {
+  // request is an object with lots of info about the request
+  // response is an object which allows us to define what kind of response we want to send back
 
-    ]
-  });
+    connection.query("SELECT * FROM task", function(err, data){
+    if(err){
+        response.status(500).json({
+            error: err
+        })
+    }
+    else {
+        response.status(200).json({
+        tasks: data
+        });
+    };
+});
+
 });
 
 
-app.post("/tasks", function (request, response) {
+// POST /developers
 
-  const newTask = request.body;
-
-  response.status(201).json({
-    message: `Successfully created task with description: ${newTask.description}, status: ${newTask.status},dueDate: ${newTask.dueDate}, dateAdded: ${newTask.date}`
-  });
-});
-
-
-
-app.put("/tasks/:id", function (request, response) {
+// PUT /developers
+app.put("/tasks/:id", function(request, response) {
 
   const updatedTask = request.body;
   const id = request.params.id;
 
-  response.status(200).json({
-    message: `Successfully updated task ID ${id} with description: ${updatedTask.description}, status: ${updatedTask.status},dueDate: ${updatedTask.dueDate}, dateAdded: ${updatedTask.date}`
-  });
+  connection.query("UPDATE task SET ? WHERE id=?", [updatedTask, id], function(err, data){
+
+    if(err){
+      response.status(500).json({
+          error: err
+      })
+  }
+  else {
+      response.status(200).json({
+      developers: data
+      });
+  };
+});
+
 });
 
 
-app.delete("/tasks/:id", function (request, response) {
 
-  const id = request.params.id;
-  
-  response.status(200).json({
-    message: 'Successfully deleted task ID ${id}'
-  });
-});
+// DELETE /developers
 
 module.exports.app = serverlessHttp(app);
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const serverlessHttp = require("serverless-http");
+// const bodyParser = require("body-parser");
+// const mysql = require("mysql");
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: "todoDB"
+// });
+
+// app.get("/tasks", function(request, response) {
+//   // request is an object with lots of info about the request
+//   // response is an object which allows us to define what kind of response we want to send back
+
+//     connection.query("SELECT * FROM task", function(err, data){
+//     if(err){
+//         response.status(500).json({
+//             error: err
+//         })
+//     }
+//     else {
+//         response.status(200).json({
+//         developers: data
+//         });
+//     };
+// });
+
+// });
+
+
+// app.post("/tasks", function (request, response) {
+
+//   const newTask = request.body;
+
+//   response.status(201).json({
+//     message: `Successfully created task with description: ${newTask.description}, status: ${newTask.status},dueDate: ${newTask.dueDate}, dateAdded: ${newTask.date}`
+//   });
+// });
+
+
+
+// app.put("/tasks/:id", function (request, response) {
+
+//   const updatedTask = request.body;
+//   const id = request.params.id;
+
+//   response.status(200).json({
+//     message: `Successfully updated task ID ${id} with description: ${updatedTask.description}, status: ${updatedTask.status},dueDate: ${updatedTask.dueDate}, dateAdded: ${updatedTask.date}`
+//   });
+// });
+
+
+// app.delete("/tasks/:id", function (request, response) {
+
+//   const id = request.params.id;
+  
+//   response.status(200).json({
+//     message: 'Successfully deleted task ID ${id}'
+//   });
+// });
+
+// module.exports.app = serverlessHttp(app);
