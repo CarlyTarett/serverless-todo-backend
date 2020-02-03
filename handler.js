60162ad047d9f9fde3ce3f2e1611fe381025051c
Carly Tarett <carlytarett@hotmail.com>
2020-02-03 15:40:24 +0000

    Updated put id to taskId

--- handler.js
+++ handler.js
@@ -39,13 +39,33 @@ app.get("/tasks", function(request, response) {
 
 // POST /developers
 
+app.post("/tasks", function(request, response) {
+
+    const newTask = request.body;
+  
+    connection.query("INSERT INTO task SET ?", [newTask], function(err, data){
+  
+      if(err){
+        response.status(500).json({
+            error: 
+            err
+                    })
+    }
+    else {
+        newTask.id = data.insertId;
+        response.status(201).json(newTask);
+    }
+  });
+  
+  });
+
 // PUT /developers
 app.put("/tasks/:id", function(request, response) {
 
   const updatedTask = request.body;
   const id = request.params.id;
 
-  connection.query("UPDATE task SET ? WHERE id=?", [updatedTask, id], function(err, data){
+  connection.query("UPDATE task SET ? WHERE taskId=?", [updatedTask, id], function(err, data){
 
     if(err){
       response.status(500).json({
@@ -65,78 +85,21 @@ app.put("/tasks/:id", function(request, response) {
 
 // DELETE /developers
 
-module.exports.app = serverlessHttp(app);
+app.delete("/tasks/:id", function(request, response) {
 
-
-
-
-
-// const express = require("express");
-// const cors = require("cors");
-// const serverlessHttp = require("serverless-http");
-// const bodyParser = require("body-parser");
-// const mysql = require("mysql");
-
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: "todoDB"
-// });
-
-// app.get("/tasks", function(request, response) {
-//   // request is an object with lots of info about the request
-//   // response is an object which allows us to define what kind of response we want to send back
-
-//     connection.query("SELECT * FROM task", function(err, data){
-//     if(err){
-//         response.status(500).json({
-//             error: err
-//         })
-//     }
-//     else {
-//         response.status(200).json({
-//         developers: data
-//         });
-//     };
-// });
-
-// });
-
-
-// app.post("/tasks", function (request, response) {
-
-//   const newTask = request.body;
-
-//   response.status(201).json({
-//     message: `Successfully created task with description: ${newTask.description}, status: ${newTask.status},dueDate: ${newTask.dueDate}, dateAdded: ${newTask.date}`
-//   });
-// });
-
-
-
-// app.put("/tasks/:id", function (request, response) {
-
-//   const updatedTask = request.body;
-//   const id = request.params.id;
-
-//   response.status(200).json({
-//     message: `Successfully updated task ID ${id} with description: ${updatedTask.description}, status: ${updatedTask.status},dueDate: ${updatedTask.dueDate}, dateAdded: ${updatedTask.date}`
-//   });
-// });
-
-
-// app.delete("/tasks/:id", function (request, response) {
-
-//   const id = request.params.id;
+    const id = request.params.id;
   
-//   response.status(200).json({
-//     message: 'Successfully deleted task ID ${id}'
-//   });
-// });
+      connection.query("Delete FROM task WHERE taskId = ?", [id], function(err, data){
+      if(err){
+          response.status(500).json({
+              error: err
+          })
+      }
+      else {
+          response.sendStatus(200);
+      };
+  });
+  
+  });
 
-// module.exports.app = serverlessHttp(app);
+module.exports.app = serverlessHttp(app);
